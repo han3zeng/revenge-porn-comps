d9ae7df67ed3440c33ea15ecec0b41f7ed741523
Han <allen0346@gmail.com>
2020-07-28 16:00:13 +0800

    Updates

---
+++ precache-manifest.3a2c0e7027dd2c44a548072c7708c60d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0973e0071d4660bdfd8ca12ea544e12",
+    "url": "/revenge-porn/index.html"
+  },
+  {
+    "revision": "d9b42f6e0a82d7e34b72",
+    "url": "/revenge-porn/static/js/2.bd6a5d1b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/revenge-porn/static/js/2.bd6a5d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a1c6dff4c7aa0ce5889",
+    "url": "/revenge-porn/static/js/main.889c6ef9.chunk.js"
+  },
+  {
+    "revision": "13fc45fb2b2a9f0c0bd1",
+    "url": "/revenge-porn/static/js/runtime-main.d58745ae.js"
+  },
+  {
+    "revision": "3eab666be6337669e2637c1c2e93ce29",
+    "url": "/revenge-porn/static/media/annotation-active-dark.3eab666b.svg"
+  },
+  {
+    "revision": "34c5c3861aff60516539f884411302fc",
+    "url": "/revenge-porn/static/media/annotation-active-light.34c5c386.svg"
+  },
+  {
+    "revision": "80a0dc67d041a99c577c3fcdd92e94d6",
+    "url": "/revenge-porn/static/media/annotation-default-dark.80a0dc67.svg"
+  },
+  {
+    "revision": "63f484cfeae6d89674889130972c6c38",
+    "url": "/revenge-porn/static/media/annotation-default-light.63f484cf.svg"
+  },
+  {
+    "revision": "ee6aaa0a654c479ee4473e72a9cd277d",
+    "url": "/revenge-porn/static/media/hamburger.ee6aaa0a.svg"
+  },
+  {
+    "revision": "5a3a4b3c7354f26e9ee613b70aaed462",
+    "url": "/revenge-porn/static/media/nav-cross.5a3a4b3c.svg"
+  },
+  {
+    "revision": "5843d3796c659c6470672dcafd396317",
+    "url": "/revenge-porn/static/media/psharp-logo-footer.5843d379.svg"
+  },
+  {
+    "revision": "340ef6c6ca5298b60cadca2131756bc3",
+    "url": "/revenge-porn/static/media/psharp-logo.340ef6c6.svg"
+  },
+  {
+    "revision": "199869ff04bc88852079cde4b85001cd",
+    "url": "/revenge-porn/static/media/pts-logo-footer.199869ff.svg"
+  },
+  {
+    "revision": "cf3ce7f3f59c5a8ac959d6937bc5a88f",
+    "url": "/revenge-porn/static/media/share-fb.cf3ce7f3.svg"
+  },
+  {
+    "revision": "4afdff51baab166bec070eaef7528df0",
+    "url": "/revenge-porn/static/media/share-line.4afdff51.svg"
+  },
+  {
+    "revision": "f36cc1cce7734b34d5ebd18672ccbf1f",
+    "url": "/revenge-porn/static/media/user-report-icon.f36cc1cc.svg"
+  }
+]);
